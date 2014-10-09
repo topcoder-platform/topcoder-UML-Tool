dd42366a8226325f9dd60f45afaee59fb747e71c
Justin Gasper <jmgasper@gmail.com>
2014-10-09 12:01:18 -0500

    Updates for working build on Windows

--- installer/run.cmd
+++ installer/run.cmd
@@ -3,8 +3,12 @@ setlocal
 
 cd /d "%~dp0"
 
+setlocal EnableDelayedExpansion
+for /L %%n in (1 1 500) do if "!__cd__:~%%n,1!" neq "" set /a "len=%%n+1"
+setlocal DisableDelayedExpansion
+
 set CP=.
-for /r ./lib %%V IN (*.jar) DO call :pathmunge "%%V"
+for /r ./lib %%V IN (*.jar) DO call :jarmunge "%%V"
 call :pathmunge .\resources
 call :pathmunge .\conf
 
@@ -15,3 +19,11 @@ goto :eof
 
 :pathmunge
 set CP=%CP%;%1
+goto :eof
+
+:jarmunge
+set absPath=%1
+setlocal EnableDelayedExpansion
+set "relPath=!absPath:~%len%!"
+set path=".!relPath!
+endlocal & set CP=%CP%;%path%
